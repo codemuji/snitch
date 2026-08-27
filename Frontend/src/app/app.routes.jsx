5179f93e14d742bc18a5ee2f32b206fdf0259db7
codemuji <rajatpradhan011@gmail.com>
2026-08-27 08:09:08 +0530

    feat: initialize frontend project with React, React Router, and Tailwind configuration

---
+++ Frontend/src/app/app.routes.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router"
+import App from "./App"
+
+export const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1>Hi this is the starting of the snitch website </h1>
+    }
+])
