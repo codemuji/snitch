7bcbdb0dc51eed18acca34f79add310a51b48465
codemuji <rajatpradhan011@gmail.com>
2026-09-02 08:15:49 +0530

    feat: implement user authentication system with email, password, and Google OAuth support

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -11,7 +11,8 @@ router.post("/login", validateLoginUser, login);
 
 router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 router.get("/google/callback", passport.authenticate("google", {
-    session: false
+    session: false,
+    failureRedirect: "http://localhost:5173/login",
 }),
     googleCallback)
 
